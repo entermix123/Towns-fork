6ffc55891de6b4c3bfc0cb68a283f65a4b0faa76
entermix123 <balabanovjordan@gmail.com>
2024-09-30 18:16:42 +0300

    Improved styling and effects for the functionality to add a new town as styler

--- towns.js
+++ towns.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#btnDelete").click(deleteTown);
     $("#btnAdd").click(addTown);
-	$('#btnShuffle').click(shuffleTowns);
+    $("#btnShuffle").click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -39,4 +39,24 @@ function shuffleTowns() {
             array[j] = oldElement;
         }
     }
+}
+
+function deleteTown() {
+    let townName = $("#townName").val();
+    $("#townName").val("");
+    let removed = false;
+    for (let option of $("#towns option")) {
+        if (option.textContent == townName) {
+            removed = true;
+            option.remove();
+        }
+    }
+    if (removed) showMessage(townName + " deleted.");
+    else showMessage(townName + " not found.");
+}
+function showMessage(msg) {
+    $("#result").text(msg).css("display", "block");
+    setTimeout(function () {
+        $("#result").hide("blind", {}, 500);
+    }, 3000);
 }
