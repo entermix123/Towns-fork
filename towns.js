cf388cfc29cc474e554db495f581217ce5f31298
entermix123 <balabanovjordan@gmail.com>
2024-09-30 19:11:18 +0300

    Improved the UI: added CSS styles + HTML structure + JS effects

--- towns.js
+++ towns.js
@@ -4,20 +4,6 @@ $(document).ready(function () {
     $("#btnShuffle").click(shuffleTowns);
 });
 
-function deleteTown() {
-    let townName = $("#townName").val();
-    $("#townName").val("");
-    let removed = false;
-    for (let option of $("#towns option")) {
-        if (option.textContent == townName) {
-            removed = true;
-            option.remove();
-        }
-    }
-    if (removed) $("#result").text(townName + " deleted.");
-    else $("#result").text(townName + " not found.");
-}
-
 function addTown() {
     let townName = $("#townNameForAdd").val();
     $("#townNameForAdd").val("");
